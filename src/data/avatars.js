6d65c1b6acd0e4a832759dc2435435ef8d3ac2a4
ShriyaAl <alladishriya05@gmail.com>
2025-12-26 01:19:24 +0530

    Fixed Login Page and set avatar to load

---
+++ src/data/avatars.js
@@ -0,0 +1,14 @@
+// Example: src/data/avatars.js
+
+export const localAvatars = [
+  {
+    id: "granny_default",
+    name: "Granny Default",
+    model_path: "../../models/avatars/granny_intro.glb", 
+  },
+  {
+    id: "ninja_default",
+    name: "Ninja Default",
+    model_path: "../../models/avatars/ninja_intro.glb",
+  }
+];
